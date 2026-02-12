ec2e2e4d15ecdeeb744af7da32e8cf708fec0dc3
Madis Pink <mail@madis.pink>
2026-02-12 16:00:24 +0200

    fix: only call setFailed in entry points

--- src/invoke-session/index.ts
+++ src/invoke-session/index.ts
@@ -2,7 +2,6 @@ import {
   getState,
   info,
   saveState,
-  setFailed,
   setOutput,
   warning
 } from '@actions/core'
@@ -17,9 +16,7 @@ export async function invokeSession(inputs: InvokeSessionInputs) {
     const args = ['start-session', '--json']
 
     if (inputs.token === undefined && process.env['EW_API_TOKEN'] === undefined) {
-      warning('api-token or EW_API_TOKEN env var must be specified')
-      setFailed('api-token or EW_API_TOKEN env var must be specified')
-      return
+      throw new Error('api-token or EW_API_TOKEN env var must be specified')
     }
 
     if (inputs.token !== undefined) {
@@ -115,7 +112,7 @@ export async function invokeSession(inputs: InvokeSessionInputs) {
     ewCli.unref()
   } catch (e) {
     warning(`ew-cli invoke failed: ${e}`)
-    setFailed(e)
+    throw e
   }
 }
 
--- src/use-emulator/main.ts
+++ src/use-emulator/main.ts
@@ -3,6 +3,7 @@ import { getInput, setFailed, warning } from '@actions/core'
 import setupEwCli from '../setup-ew-cli/index.js'
 import { invokeSession } from '../invoke-session/index.js'
 import { getInvokeSessionInputs } from '../invoke-session/inputs.js'
+import { extractErrorMessage } from '../lib/utils.js'
 
 
 (async () => {
@@ -10,9 +11,12 @@ import { getInvokeSessionInputs } from '../invoke-session/inputs.js'
     const version = getInput('version')
     await setupEwCli(version)
     await invokeSession(getInvokeSessionInputs())
+    // TODO(madis) I'm surprised this works,
+    // we need a direct exit here to "leak" the ew-cli process
     process.exit(0)
   } catch (error) {
-    warning(`use-emulator failed: ${error}`)
-    setFailed(error)
+    const msg = extractErrorMessage(error)
+    warning(`use-emulator failed: ${msg}`)
+    setFailed(msg)
   }
 })()
