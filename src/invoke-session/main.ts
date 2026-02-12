24302c4503efa261f790cee075766983f3e7b2b0
Madis Pink <mail@madis.pink>
2026-02-12 15:24:55 +0200

    fix: proc exit location

--- src/invoke-session/main.ts
+++ src/invoke-session/main.ts
@@ -5,9 +5,9 @@ import { setFailed, warning } from '@actions/core'
 (async () => {
   try {
     await invokeSession(getInvokeSessionInputs())
+    process.exit(0)
   } catch (error) {
     warning(`invoke-session failed: ${error}`)
     setFailed(error)
   }
-  process.exit(0)
 })()
