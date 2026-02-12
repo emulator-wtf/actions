251f599eb1545b08b23c5e7f0516e80b6f2cc8ed
Madis Pink <mail@madis.pink>
2026-02-12 15:18:31 +0200

    chore: fix: don't exit process immediately

--- src/invoke-session/main.ts
+++ src/invoke-session/main.ts
@@ -2,10 +2,12 @@ import { invokeSession } from './index.js'
 import { getInvokeSessionInputs } from './inputs.js'
 import { setFailed } from '@actions/core'
 
-try {
-  invokeSession(getInvokeSessionInputs())
-} catch (error) {
-  warning(`invoke-session failed: ${e}`)
-  setFailed(e)
-}
-process.exit(0)
+(async () => {
+  try {
+    await invokeSession(getInvokeSessionInputs())
+  } catch (error) {
+    warning(`invoke-session failed: ${e}`)
+    setFailed(e)
+  }
+  process.exit(0)
+})()
