70a106c9c39008043862964110fddee4e44ddd69
Madis Pink <mail@madis.pink>
2026-02-13 15:08:32 +0200

    chore: fix use-emulator lint

--- src/use-emulator/main.ts
+++ src/use-emulator/main.ts
@@ -6,17 +6,15 @@ import { getInvokeSessionInputs } from '../invoke-session/inputs.js'
 import { extractErrorMessage } from '../lib/utils.js'
 
 
-(async () => {
-  try {
-    const version = getInput('version')
-    await setupEwCli(version)
-    await invokeSession(getInvokeSessionInputs())
-    // TODO(madis) I'm surprised this works,
-    // we need a direct exit here to "leak" the ew-cli process
-    process.exit(0)
-  } catch (error) {
-    const msg = extractErrorMessage(error)
-    warning(`use-emulator failed: ${msg}`)
-    setFailed(msg)
-  }
-})()
+try {
+  const version = getInput('version')
+  await setupEwCli(version)
+  await invokeSession(getInvokeSessionInputs())
+  // TODO(madis) I'm surprised this works,
+  // we need a direct exit here to "leak" the ew-cli process
+  process.exit(0)
+} catch (error) {
+  const msg = extractErrorMessage(error)
+  warning(`use-emulator failed: ${msg}`)
+  setFailed(msg)
+}
